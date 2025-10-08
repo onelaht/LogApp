0395613ec1aa5a840292aa2cb6bb89e1d86a1ba2
onelaht <covercoverunder@gmail.com>
2025-10-08 10:56:38 -0400

    docs and implemented ui for set filtering

--- src/Filters/FilterCheckboxSet.tsx
+++ src/Filters/FilterCheckboxSet.tsx
@@ -1,11 +1,48 @@
-import React, {useEffect} from 'react';
+// react
+import React, {useCallback, useEffect, useState} from 'react';
+// ag grid
 import {CustomFilterProps} from "ag-grid-react";
+// global vars
+import {useFilter} from "../Providers/ProviderFilter";
+// mui components
+import {Button, Checkbox, FormControlLabel, FormGroup} from "@mui/material";
+// styling
+import '../Filters/FilterCheckboxSet.css'
 
 export default function FilterCheckboxSet ({model, onModelChange, getValue, colDef}: CustomFilterProps) {
 
-    useEffect(():void => {
-        console.log(model, onModelChange, getValue, colDef);
+    const { retrieveColSet } = useFilter();
+    const [set, setSet] = useState<string[] | null>(null)
+
+    const handleChange = useCallback((e:any) => {
+        console.log(e?.target?.checked);
     }, [])
 
-    return <>Fuck you :)</>
+    useEffect(():void => {
+        setSet(retrieveColSet(colDef?.field))
+    }, [])
+
+    return (
+        <div className="FormContainer">
+            <FormGroup className="FormGroupContainer">
+                <FormControlLabel
+                    control={
+                        <Checkbox size="small" onChange={handleChange}/>
+                    }
+                    label="All"
+                />
+                {set && set.map((att:string|null) => (
+                    <FormControlLabel
+                        control={
+                            <Checkbox size="small" onChange={handleChange}/>
+                        }
+                        label={att}
+                    />
+                ))}
+            </FormGroup>
+                <Button className="ButtonSpacing"
+                 variant="outlined"
+                >Reset</Button>
+        </div>
+    )
 }
