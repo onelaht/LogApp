69003563522735bdba078dd1f7a87f2f684a8614
onelaht <covercoverunder@gmail.com>
2025-10-10 11:29:24 -0400

    Checkbox logic initialization

--- src/Filters/FilterCheckboxSet.tsx
+++ src/Filters/FilterCheckboxSet.tsx
@@ -1,5 +1,5 @@
 // react
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 // ag grid
 import {CustomFilterCallbacks, CustomFilterProps, useGridFilter} from "ag-grid-react";
 // global vars
@@ -13,7 +13,7 @@ import {DoesFilterPassParams} from "ag-grid-community";
 export default function FilterCheckboxSet ({model, onModelChange, getValue, colDef}: CustomFilterProps) {
 
     const { retrieveColSet } = useFilter();
-    const [set] = useState(retrieveColSet(colDef?.field))
+    const [set] = useState<string[] | null>(retrieveColSet(colDef?.field))
     const [map, setMap] = useState(new Map<string, boolean>());
 
     // initialize map
@@ -25,25 +25,73 @@ export default function FilterCheckboxSet ({model, onModelChange, getValue, colD
         setMap(tempMap);
     }, [])
 
-    const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>)=> {
-        if(e.target.id === "all") {
+    const resetMap:() => void = useCallback(():void => {
+        setMap(prev => {
+            const newMap = new Map(map);
+            set?.forEach((v) => {
+                newMap.set(v, true)
+            })
+            return newMap;
+        })
+    }, [map, set])
 
-        }
-    }, [])
+    const ifAllTrue = useMemo(():boolean => {
+        let state:boolean = true;
+        map.forEach((v) => {
+            if (!v) state = false;
+        })
+        return state;
+    }, [map])
+
+    const ifEnabled = useCallback((e:any) => {
+        console.log(e);
+    }, [map])
+
+    const modifyMap = useCallback((key:string):void => {
+        //
+        if(!map.has(key)) return;
+        //
+        setMap(prev => {
+            const newMap = new Map(prev);
+            newMap.set(key, !newMap.get(key));
+            return newMap;
+        })
+    }, [map])
+
+    const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>)=> {
+        if(e.target.id === "all")
+            resetMap();
+        else
+            modifyMap(e.target.id);
+    }, [modifyMap, resetMap])
+
+
+    useEffect(() => {
+        console.log(map)
+    }, [map])
 
     return (
         <div className="FormContainer">
             <FormGroup className="FormGroupContainer">
                 <FormControlLabel
                     control={
-                        <Checkbox id="all" size="small" onChange={handleChange}/>
+                        <Checkbox
+                            id="all"
+                            size="small"
+                            onChange={handleChange}
+                            checked={ifAllTrue}
+                        />
                     }
                     label="All"
                 />
-                {set && set.map((att:string|null) => (
+                {set && set.map((att:string) => (
                     <FormControlLabel
                         control={
-                            <Checkbox size="small" onChange={handleChange}/>
+                            <Checkbox
+                                id={att}
+                                size="small"
+                                onChange={handleChange}
+                            />
                         }
                         label={att}
                     />
