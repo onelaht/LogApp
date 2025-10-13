acbc84b1d7c136d4ea408f94df86d83982dcc670
onelaht <covercoverunder@gmail.com>
2025-10-13 11:53:04 -0400

    Docs

--- src/Filters/FilterCheckboxSet.tsx
+++ src/Filters/FilterCheckboxSet.tsx
@@ -11,23 +11,31 @@ import {Checkbox, FormControlLabel, FormGroup} from "@mui/material";
 import '../Filters/FilterCheckboxSet.css'
 
 export default function FilterCheckboxSet ({onModelChange, colDef}: CustomFilterProps) {
-
+    // retrieve unique sets for column
     const { retrieveColSet } = useFilter();
+    // determines which set is enabled/disabled
     const [map, setMap] = useState(new Map<string, boolean>());
 
     // initialize map
     useEffect(() => {
         const tempMap = new Map<string, boolean>();
+        // retrieve each col set and show all
         retrieveColSet(colDef?.field)?.forEach((v) => {
             tempMap.set(v, true)
         })
         setMap(tempMap);
     }, [])
 
+    // retrieves state for specified set
+    // - returns true if set is enabled
+    // - returns false if set is disabled
     const ifTrue = useCallback((key:string):boolean => {
         return !!(map.get(key));
     }, [map])
 
+    // retrieves state of all sets
+    // - returns true if all are enabled
+    // - returns false if at least one is disabled
     const ifAllTrue = useMemo(():boolean => {
         let status = true;
         map.forEach((v:boolean) => {
@@ -36,8 +44,12 @@ export default function FilterCheckboxSet ({onModelChange, colDef}: CustomFilter
         return status;
     }, [map])
 
+    // modifies state for specified set
+    // - uses key value for O(1) lookup and assignment
     const modifyKey = useCallback((key:string):void => {
+        // if not found in map, leave
         if(!map.has(key)) return;
+        // create new instance, assign, and update map
         setMap(prev => {
             const newMap = new Map(prev);
             newMap.set(key, !newMap.get(key));
@@ -45,7 +57,10 @@ export default function FilterCheckboxSet ({onModelChange, colDef}: CustomFilter
         })
     }, [map])
 
+    // modifies state of all set
+    // - utilizes boolean parameter to assign state
     const modifyAllKeys = useCallback((state: boolean):void => {
+        // create new instance, loop through all set, assign, and update map
         setMap(prev => {
             const newMap = new Map(prev);
             Array.from(newMap.keys()).forEach((key) => {
@@ -56,17 +71,26 @@ export default function FilterCheckboxSet ({onModelChange, colDef}: CustomFilter
     }, [])
 
     const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        // all checkbox
+        // - if checked, show all sets
+        // - otherwise, hide all sets
         if(e.target.id === "all")
             e.target.checked ? modifyAllKeys(true) : modifyAllKeys(false);
+        // toggle boolean condition via key value (if exists)
         else
             modifyKey(e.target.id);
+        // update ag grid filter model
         onModelChange(e.target.id ? map : null);
     }, [modifyAllKeys, modifyKey, onModelChange, map])
 
-    const doesFilterPass = useCallback(({data, node}:DoesFilterPassParams) => {
+    // determine which nodes to show
+    // - compares all nodes based on boolean condition
+    // - returns boolean status via key lookup
+    const doesFilterPass = useCallback(({data}:DoesFilterPassParams) => {
         return ifTrue(data?.[colDef.field as string])
     }, [colDef, ifTrue])
 
+    // call hook (apply new filtered rows)
     useGridFilter({doesFilterPass});
 
     return (
