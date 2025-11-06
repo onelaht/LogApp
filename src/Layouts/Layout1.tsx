689bce967e777ab0298e193c3ee95610b4eccc81
onelaht <covercoverunder@gmail.com>
2025-11-06 11:31:29 -0500

    Add LogColVisibility.tsx to flexlayout template

--- src/Layouts/Layout1.tsx
+++ src/Layouts/Layout1.tsx
@@ -7,8 +7,9 @@ export const Layout1 = {
             children: [
                 {
                     type: "tab",
-                    name: "Test",
-                    component: "Placeholder",
+                    name: "Column Visibility",
+                    component: "ColumnVisibility",
+                    borderWidth: 250,
                 }
             ]
         }
