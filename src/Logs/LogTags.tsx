b572428b3658a1c4e006fb8e6967b8d83a2fa3b3
onelaht <covercoverunder@gmail.com>
2025-11-18 15:53:13 -0500

    Update grid and column list based on tag revision

--- src/Logs/LogTags.tsx
+++ src/Logs/LogTags.tsx
@@ -1,5 +1,11 @@
-// global var
+// react
+import React, {ChangeEvent, useCallback, useState} from "react";
+// global vars
 import {useTag} from "../Providers/ProviderTag";
+import {useGrid} from "../Providers/ProviderGrid";
+// types
+import {Row} from "../Types/Row";
+import {ColDef, ISelectCellEditorParams} from "ag-grid-community";
 // mui commponents
 import {
     Accordion,
@@ -13,26 +19,23 @@ import {
 } from '@mui/material';
 // mui icons
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import {useCallback, useState} from "react";
-import {useGrid} from "../Providers/ProviderGrid";
-import {Row} from "../Types/Row";
-import {ColDef, ISelectCellEditorParams} from "ag-grid-community";
 
 export default function LogTags() {
+    // global vars
     const {setColDefs} = useGrid();
     const {tagDefs, setTagDefs} = useTag();
+    // track tags with edit mode enabled
+    const [edit, setEdit] = useState<string>("");
+    // assign tag and params (new tags)
     const [tagName, setTagName] = useState<string>("");
     const [parameters, setParameters] = useState<string[]>([""]);
+    // assign tag and params (existing tags)
+    const [draftTagName, setDraftTagName] = useState<string>("");
+    const [draftParameters, setDraftParameters] = useState<string[]>([""]);
 
-    const handleParamChange = useCallback((value :string, index: number) => {
-        const updateParams = parameters.map((p, i) => {
-            if(i === index) return value;
-            return p;
-        })
-        setParameters(updateParams);
-    }, [parameters])
-
-    const handleCreateTag = useCallback(() => {
+    // build new tag and push to column list
+    const handleNewTag = useCallback(() => {
+        // build new tag
         const col:ColDef<Row> = {
             field: tagName,
             editable: true,
@@ -55,8 +58,9 @@ export default function LogTags() {
             tempDefs.push(col);
             return tempDefs;
         })
-    }, [tagName, parameters, tagDefs, setColDefs])
+    }, [tagName, parameters, setColDefs, setTagDefs])
 
+    // removes tag from column list
     const handleDeleteTag = useCallback((col:ColDef<Row>) => {
         setTagDefs(prev => {
             return [...prev].filter((i) => i !== col);
@@ -65,6 +69,57 @@ export default function LogTags() {
             return [...prev].filter((i) => i !== col);
         })
     }, [setColDefs, setTagDefs])
+
+    // update parameter value when updated
+    const handleParametersChange = (value: string, index: number, handler:React.Dispatch<React.SetStateAction<string[]>>) => {
+        handler(prev => {
+            return prev.map((p:string, i:number) => {
+                if(index === i) return value;
+                return p;
+            })
+        })
+    }
+
+    // update edit tracker and copy current tag name/params for revision
+    const handleEditState = useCallback((col:ColDef<Row>, ) => {
+        (col.field === edit) ? setEdit("") : setEdit(col.field as string);
+        // initialize draft values from current definition
+        setDraftTagName(col.field as string);
+        setDraftParameters(() => {
+            return [...col.cellEditorParams.values];
+        })
+    }, [edit])
+
+    // update tag based on user input and update column list
+    const handleExistingTag = useCallback((oldTag:ColDef<Row>) => {
+        // build revised tag
+        const newTag:ColDef<Row> = {
+            field: draftTagName,
+            editable: true,
+            cellDataType: "text",
+            filter: "agTextColumnFilter",
+            cellEditor: 'agSelectCellEditor',
+            cellEditorParams: {
+                values: draftParameters
+            } as ISelectCellEditorParams,
+        }
+        // update coldefs
+        setColDefs(prev => {
+            return prev.map((i) => {
+                if (i.field === oldTag.field)
+                    return newTag;
+                return i;
+            });
+        })
+        // update tag tracker
+        setTagDefs(prev => {
+            return prev.map((i) => {
+                if(i.field === oldTag.field)
+                    return newTag;
+                return i;
+            })
+        })
+    }, [draftTagName, draftParameters, setColDefs, setTagDefs])
 
     return (
         <Box sx={{m:2}}>
@@ -78,48 +133,68 @@ export default function LogTags() {
                         <Typography variant="body2">No Tags Found</Typography>
                     }
                     {tagDefs.length >= 1 && tagDefs.map((i) => (
+
                        <Accordion>
                            <AccordionSummary
                                expandIcon={<ExpandMoreIcon/>}>
                                <Typography variant="body1">{i.field}</Typography>
                            </AccordionSummary>
                            <AccordionDetails>
+                               <Box sx={{pb: 1, display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
+                                   <Button
+                                       sx={{ml: 1, mr: 1}}
+                                       size="small"
+                                       onClick={() => handleEditState(i)}>
+                                       {edit === i.field as string ? "Disable Edit" : "Enable Edit"}
+                                   </Button>
+                                   <Button
+                                       sx={{ml: 1, mr: 1}}
+                                       size="small"
+                                       onClick={() => handleDeleteTag(i)}>
+                                       Delete
+                                   </Button>
+                               </Box>
                                <Box sx={{m: 1}}>
                                    <TextField
                                        label="Tag Name"
-                                       value={i.field}/>
+                                       disabled={edit !== i.field as string}
+                                       onChange={(e:ChangeEvent<HTMLInputElement>) => setDraftTagName(e.target.value)}
+                                       value={edit !== i.field ? i.field : draftTagName}/>
                                </Box>
                                <Divider sx={{ml: 1, mr: 1, mt: 2, mb: 2}}/>
                                <Box sx={{m: 1, display: "flex", flexDirection: "column", justifyContent: "center"}}>
-                                   {i.cellEditorParams.values.map((j:string, index: number) => (
+                                   {edit === i.field as string && draftParameters.map((j:string, index: number) => (
                                        <TextField
                                            sx={{pb: 1.5}}
+                                           disabled={false}
+                                           onChange={(e) => handleParametersChange(e.target.value, index, setDraftParameters)}
+                                           label={`Parameter ${index+1}`}
+                                           value={j}/>
+                                   ))}
+                                   {edit !== i.field as string && i.cellEditorParams.values.map((j:string, index: number) => (
+                                       <TextField
+                                           sx={{pb: 1.5}}
+                                           disabled={true}
                                            label={`Parameter ${index+1}`}
                                            value={j}/>
                                    ))}
                                </Box>
-                               <Box sx={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
-                                   <Button
-                                       sx={{ml: 1, mr: 1}}
-                                       size="small"
-                                       variant="outlined">
-                                       Edit
-                                   </Button>
-                                   <Button
-                                       sx={{ml: 1, mr: 1}}
-                                       size="small"
-                                       onClick={() => handleDeleteTag(i)}
-                                       variant="outlined">
-                                       Delete Tag
-                                   </Button>
-                               </Box>
+                               {edit === i.field as string &&
+                                   <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center"}}>
+                                       <Button
+                                           onClick={() => setDraftParameters(prev =>  [...prev, ""])}>
+                                           Add Parameter
+                                       </Button>
+                                       <Button
+                                           onClick={() => handleExistingTag(i)}>
+                                           Save Changes
+                                       </Button>
+                                   </Box>
+                               }
                            </AccordionDetails>
                        </Accordion>
                     ))}
                 </AccordionDetails>
-
-
-
             </Accordion>
             <Accordion defaultExpanded>
                 <AccordionSummary
@@ -140,7 +215,7 @@ export default function LogTags() {
                                 sx={{pb: 1.5}}
                                 label={`Parameter ${index+1}`}
                                 value={param}
-                                onChange={(e) => handleParamChange(e.target.value, index)}
+                                onChange={(e) => handleParametersChange(e.target.value, index, setParameters)}
                             />
                         ))}
                         <Button
@@ -150,7 +225,7 @@ export default function LogTags() {
                         <Divider sx={{ml: 1, mr: 1, mt: 2, mb: 2}}/>
                         <Button
                             variant="outlined"
-                            onClick={handleCreateTag}>
+                            onClick={handleNewTag}>
                             Create tag
                         </Button>
                     </Box>
