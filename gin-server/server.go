d78a022f90e27650296ac7d3c783bb0a4b5e406a
onelaht <covercoverunder@gmail.com>
2025-12-03 10:54:58 -0500

    Send column data to backend

--- gin-server/server.go
+++ gin-server/server.go
@@ -2,22 +2,37 @@ package main
 
 import (
 	"example/gin-server/userdata"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type DataReceived struct {
+type rawFile struct {
 	UserData string `json:"userData"`
 }
 
-// handleUpload
+type colDef struct {
+	CellDataType string              `json:"cellDataType"`
+	Field        string              `json:"field"`
+	Filter       string              `json:"filter"`
+	FilterParams map[string][]string `json:"filterParams"`
+}
+type account struct {
+	AccName string              `json:"accName"`
+	RowData []map[string]string `json:"rowData"`
+	ColDefs []colDef            `json:"colDefs"`
+	TagDefs []colDef            `json:"tagDefs"`
+}
+
+// rawUpload
 // converts raw user data (TXT) to array of tuples
 // - returns 2D array of user data
 // - returns nil if raw user data is empty
 // - returns error message if any error occurs
-func handleUpload(c *gin.Context) {
-	var data DataReceived
+func rawUpload(c *gin.Context) {
+	var data rawFile
+	// prompt if error occurs
 	if err := c.BindJSON(&data); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
@@ -30,12 +45,23 @@ func handleUpload(c *gin.Context) {
 	})
 }
 
+func saveNewAccount(c *gin.Context) {
+	var data account
+	// prompt if error occurs
+	if err := c.BindJSON(&data); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	fmt.Println(data.AccName)
+}
+
 // contains endpoint initialization and handlers
 func main() {
 	// initialize gin
 	router := gin.Default()
 	// upload endpoint handler
-	router.POST("/upload", handleUpload)
+	router.POST("/upload", rawUpload)
+	router.POST("/saveNewAccount", saveNewAccount)
 	// run via localhost:5000
 	err := router.Run(":5000")
 	// exit if any error occurs
--- src/Logs/LogToolbar.tsx
+++ src/Logs/LogToolbar.tsx
@@ -1,15 +1,21 @@
 // react
-import React, {useCallback, useEffect, useState} from "react";
+import React, {useCallback, useEffect, useMemo, useState} from "react";
 // global vars
-import { useGrid } from "../Providers/ProviderGrid";
+import {useGrid} from "../Providers/ProviderGrid";
+import {useTag} from "../Providers/ProviderTag";
 // mui components
-import {Button, Stack, styled} from "@mui/material";
+import {Button, Stack, styled, TextField} from "@mui/material";
 // mui styling
 import {LogToolbarMUI, HiddenInput} from "./LogToolbarMUI";
+// types
+import {Row} from "../Types/Row";
 
 export default function LogToolbar() {
     // global vars
-    const {setGridData} = useGrid();
+    const {setGridData, gridRef, colDefs} = useGrid();
+    const {tagDefs} = useTag();
+    // account name
+    const [accName, setAccName] = useState<string>("");
     // store user dataafalse
     const [rawString, setRawString] = useState<ArrayBuffer | string | null>(null);
     // MUI; hidden file upload form
@@ -27,6 +33,15 @@ export default function LogToolbar() {
         reader.readAsBinaryString(data);
     }, [])
 
+    const noTagDefs = useMemo(() => {
+        if(tagDefs.length < 1) return colDefs;
+        const tempDefs = [...colDefs];
+        return tempDefs.filter((i) => {
+            const idx = tagDefs.indexOf(i);
+            return idx < 0;
+        })
+    }, [colDefs, tagDefs])
+
     // initialize grid data
     useEffect(() => {
         // leave if uploaded data is empty
@@ -37,6 +52,26 @@ export default function LogToolbar() {
             setGridData(rawString);
         }
     }, [rawString, setGridData])
+
+    const saveNewAccount = useCallback(async() => {
+        if(!gridRef?.current) return;
+        // get row data
+        const rowData:Row[] = []
+        gridRef.current.api?.forEachNode((i) => {
+            if(i?.data) rowData.push(i.data)
+        });
+        // send tags, cols, and row data to backend
+        const res = await fetch("/api/saveNewAccount", {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({accName: accName, rowData: rowData, colDefs: noTagDefs, tagDefs: tagDefs})
+        })
+        // if any error occurs, prompt error msg to console
+        if (!res.ok) {
+            const text = await res.text();
+            console.error("HTTP error:", res.status, text);
+        }
+    }, [gridRef, noTagDefs, tagDefs, accName])
 
     return (
         <Stack direction="row" gap={1}>
@@ -52,6 +87,17 @@ export default function LogToolbar() {
                     onChange={(e:any) => readInFile(e.target.files?.[0])}
                     accept="csv"
                 />
+            </Button>
+            <TextField
+                size="small"
+                value={accName}
+                onChange={(e) => setAccName(e.target.value)}/>
+            <Button
+                variant="outlined"
+                size="small"
+                onClick={saveNewAccount}
+                disabled={accName.length === 0}
+                >Save account
             </Button>
         </Stack>
     )
